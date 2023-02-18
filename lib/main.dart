2d87f767c65dff8247d0205e5272077fa81fb7aa
tsunoda <tsunoda@aomori-u.ac.jp>
2023-02-18 14:13:57 +0900

    Firestoreアクセス

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -21,12 +25,39 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+late CollectionReference cref;
+
 class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  void initState() {
+    super.initState();
+    cref = FirebaseFirestore.instance.collection('root');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('テストアプリ')),
-      body: Text('test'),
+      body: buildBody(context),
+    );
+  }
+
+  Widget buildBody(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: cref.snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) return LinearProgressIndicator();
+
+        print("########## Firestore Access start");
+        snapshot.data!.docs.forEach((elem) {
+          print(elem.id);
+          print(elem.get('email'));
+        });
+        print("########## Firestore Access end");
+
+        return Text('test');
+      },
     );
   }
 }
